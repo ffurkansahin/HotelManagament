6aa34805e4c4cd999831adf083d200a97a931462
ahmetblal <bilal208@eskisehir.edu.tr>
2023-12-09 14:36:22 +0300

    test

--- src/BusinessLayer/BLL.java
+++ src/BusinessLayer/BLL.java
@@ -1,5 +1,7 @@
 package BusinessLayer;
 
 public class BLL {
-
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
--- src/Entities/Guest.java
+++ src/Entities/Guest.java
@@ -1,4 +1,5 @@
 package Entities;
+
 import java.time.LocalDate;
 
 public class Guest {
@@ -7,34 +8,42 @@ public class Guest {
     private String Surname;
     private LocalDate CheckIn;
     private LocalDate CheckOut;
-    
+
     public LocalDate getCheckIn() {
         return CheckIn;
     }
     public void setCheckIn(LocalDate checkIn) {
         CheckIn = checkIn;
     }
+
     public LocalDate getCheckOut() {
         return CheckOut;
     }
+
     public void setCheckOut(LocalDate checkOut) {
         CheckOut = checkOut;
     }
+
     public String getTC() {
         return TC;
     }
+
     public void setTC(String tC) {
         TC = tC;
     }
+
     public String getName() {
         return Name;
     }
+
     public void setName(String name) {
         Name = name;
     }
+
     public String getSurname() {
         return Surname;
     }
+
     public void setSurname(String surname) {
         Surname = surname;
     }
