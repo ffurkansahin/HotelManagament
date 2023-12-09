d11c7b53e7c8d917fa6f0f5b90e28097dab87752
ahmetblal <bilal208@eskisehir.edu.tr>
2023-12-09 14:48:33 +0300

    testblal

--- src/BusinessLayer/BLL.java
+++ src/BusinessLayer/BLL.java
@@ -3,5 +3,6 @@ package BusinessLayer;
 public class BLL {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("5 +4 =" + String.valueOf(9));
     }
 }
